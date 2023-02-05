ab3ea4989f4b30e2b53ff53645344a620be79a10
Yoshihisa Yamano <yoshihisa.yamano@gmail.com>
2023-02-05 18:34:24 +0900

    Add polybar nb settings

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -6,13 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITOR=DP-1-8 polybar --reload main &
-MONITOR=DP-1-1-8 polybar --reload secondary &
-# echo "Polybar launched..."
-# if type "xrandr"; then
-#     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#         MONITOR=$m polybar --reload example &
-#     done
-# else
-#     polybar --reload example &
-# fi
+echo "Polybar launched..."
+if [ $(hostname) == "desktop" ]; then
+    MONITOR=DP-1-8 polybar --reload main &
+    MONITOR=DP-1-1-8 polybar --reload secondary &
+elif type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload main &
+    done
+else
+    polybar --reload main &
+fi
