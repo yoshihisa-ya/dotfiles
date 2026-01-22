2b7f6b00bbcc2f7e397c28a7f788015517585455
Yoshihisa Yamano <yoshihisa.yamano@gmail.com>
2026-01-22 20:25:14 +0900

    WezTerm: Add i3 like keybind

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -20,6 +20,22 @@ return {
         window:perform_action(wezterm.action.CopyMode({ SetSelectionMode = "Line" }), pane)
       end),
     },
+    -- i3 like keybinds
+    { key = '1', mods = 'ALT', action = act.ActivateTab(0) },
+    { key = '2', mods = 'ALT', action = act.ActivateTab(1) },
+    { key = '3', mods = 'ALT', action = act.ActivateTab(2) },
+    { key = '4', mods = 'ALT', action = act.ActivateTab(3) },
+    { key = '5', mods = 'ALT', action = act.ActivateTab(4) },
+    { key = '6', mods = 'ALT', action = act.ActivateTab(5) },
+    { key = '7', mods = 'ALT', action = act.ActivateTab(6) },
+    { key = '8', mods = 'ALT', action = act.ActivateTab(7) },
+    { key = '9', mods = 'ALT', action = act.ActivateTab(8) },
+    { key = '0', mods = 'ALT', action = act.ActivateTab(-1) },
+    { key = 'h', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left' },
+    { key = 'j', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Down' },
+    { key = 'k', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up' },
+    { key = 'l', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
+    { key = 's', mods = 'ALT', action = act.PaneSelect { mode = 'SwapWithActiveKeepFocus' } },
   },
     key_tables = {
     copy_mode = {
