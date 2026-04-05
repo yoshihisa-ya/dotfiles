c6f3c8f5e027a850d010fbb065f0355409fee503
Yoshihisa Yamano <yoshihisa.yamano@gmail.com>
2026-04-06 00:11:11 +0900

    Neovim: Update git commit

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -18,7 +18,7 @@ autocmd("BufReadPost", {
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount and vim.bo.filetype ~= "gitcommit" then
+    if mark[1] > 0 and mark[1] <= lcount and not vim.bo.filetype:find("commit") then
       vim.api.nvim_win_set_cursor(0, mark)
     end
   end,
